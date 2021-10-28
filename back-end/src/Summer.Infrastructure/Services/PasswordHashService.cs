e2ea14c649d999c740c803fbe79eef31633a01aa
xhznl <xhznl@foxmail.com>
2021-10-28 11:37:00 +0800

    修改密码hash实现

--- back-end/src/Summer.Infrastructure/Services/PasswordHashService.cs
+++ back-end/src/Summer.Infrastructure/Services/PasswordHashService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Summer.Domain.Entities;
 using Summer.Domain.Interfaces;
 
@@ -9,17 +11,33 @@ namespace Summer.Infrastructure.Services
         public string Hash(User user, string password)
         {
             if (user == null) throw new ArgumentNullException(nameof(user));
-            if (password == null) throw new ArgumentNullException(nameof(password));
+            if (string.IsNullOrEmpty(user.SecurityStamp)) throw new ArgumentNullException(nameof(user));
+            if (string.IsNullOrEmpty(password)) throw new ArgumentNullException(nameof(password));
 
-            return BCrypt.Net.BCrypt.HashPassword(user.SecurityStamp + password);
+            password = user.SecurityStamp + password;
+
+            // generate a 128-bit salt using a cryptographically strong random sequence of nonzero values
+            var salt = new byte[128 / 8];
+            using (var rngCsp = new RNGCryptoServiceProvider())
+            {
+                rngCsp.GetNonZeroBytes(salt);
+            }
+
+            // derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
+            var hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            return hashed;
         }
 
         public bool Verify(User user, string password)
         {
-            if (user == null) throw new ArgumentNullException(nameof(user));
-            if (password == null) throw new ArgumentNullException(nameof(password));
-
-            return BCrypt.Net.BCrypt.Verify(user.SecurityStamp + password, user.PasswordHash);
+            var hashed = Hash(user, password);
+            return hashed == user.PasswordHash;
         }
     }
 }
